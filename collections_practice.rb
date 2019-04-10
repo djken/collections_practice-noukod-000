765fc6485988e8138e1b004c52ff1adc76acfcd1
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-10 12:18:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,6 +22,6 @@ end
 #Question 4
 array = ["blake", "ashley", "scott"]
 def swap_elements(array, index, destination_index)
-  array[index], array[destination_index] = array[destination_index], array[index]
+  array[1], array[3] = array[3], array[2]
   array
 end
