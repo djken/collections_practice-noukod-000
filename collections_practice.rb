487715662966736d7a358cb08ff9059e22aa7dd9
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-10 12:56:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -55,10 +55,6 @@ end
 #Question 9
 def add_s(add_s_array)
    add_s_array.map.with_index do |word, index|
-    if index == 1 
-      word 
-    else 
-      word + "s"
-    end
+    if index == 1 ? word : word + "s" word
   end
 end
