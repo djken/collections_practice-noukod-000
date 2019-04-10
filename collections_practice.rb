1def6166459019b37cffbfce0a9a5912161423ab
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-10 12:59:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,7 +54,7 @@ end
 
 #Question 9
 def add_s(add_s_array)
-   add_s_array.map.with_index do |word, index|
-    index == 1 ? word : word + "s"
+   add_s_array.map.with_index do |element, index|
+    index == 1 ? element : element + "s"
   end
 end
