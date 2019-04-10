d229fdab9cdf451d105d40d5b9ce87438f2d7066
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-10 12:28:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,14 +20,7 @@ end
 end
 
 #Question 4
-# array = ["blake", "ashley", "scott"]
-# def swap_elements(array, index, destination_index)
-#   array[], array[destination_index] = array[destination_index], array[index]
-# end
-string_test = ["blake", "ashley", "scott"]
-#swap_elements(test_array)
-
-def swap_elements_from_to(array, index, new_index)
-  array[index], array[new_index] = array[new_index], array[index]
-  return array
+array = ["blake", "ashley", "scott"]
+def swap_elements(array, index, destination_index)
+  array[], array[destination_index] = array[destination_index], array[index]
 end
