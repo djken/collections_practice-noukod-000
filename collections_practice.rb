18014633ec1260d78489411c9c0e9bc7b0f9cab9
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-10 11:51:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(int_array)
+  int_array.sort do |a, b|
+  a <=> b
+  end
+end
+
+def sort_array_desc(int_array)
+  int_array.sort.reverse do |a, b|
+  a <=> b
+end
+end
+
+def sort_array_char_count(string_array)
+  string_array.sort do |a, b|
+  a.size <=> b.size
+end
+end
 
